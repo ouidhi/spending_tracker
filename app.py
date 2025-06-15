538a47df80e08dc5c16905e5472c1e9ff2a051a9
ouidhi <136058938+ouidhi@users.noreply.github.com>
2025-06-15 16:41:36 -0400

    Update app.py

--- app.py
+++ app.py
@@ -22,28 +22,6 @@ def preprocess(df):
     df['Amount'] = pd.to_numeric(df['Amount'])
     return df
 
-def categorizer(desc):
-    if re.search(r"(shoppers|rexall|pharmacy|drug)", desc):
-        return "Pharmacy"
-    elif re.search(r"(starbucks|timhortons|coffee|cafe|tea|matcha|ippodo|espressobar|espresso|varda|dean|arabica|caf|tsujiri|milkys|alley)", desc):
-        return "Cafe"
-    elif re.search(r"(walmart|loblaws|nofrills|grocery|metro|johndanielles|market|hmart|fortinos|subzimandi|7 eleven|convenience|eleven)", desc):
-        return "Groceries"
-    elif re.search(r"(ubertrip|lyft|taxi|presto|shell|petro)", desc):
-        return "Transport"
-    elif re.search(r"(netflix|spotify|subscription|disneyplus|bill|insurance|virginplus)", desc):
-        return "Subscription"
-    elif re.search(r"(payment|credit|thankyou|cashback)", desc):
-        return "Credit Card Payment"
-    elif re.search(r"(restaurant|dining|pizza|mcdonalds|burger|waffle|eats|chicken|kellyslanding|dairyqueen|bakery|bistro|popeyes|chatime|chipotle|brewhouse|mexicana|kitc|sweets|craveables|baskinrobbins|ice|hakkalicious|villacaledon|kfc|shanghai|turtlejacks|beavertails|food|earls|krispykreme|tacos|blueclaw|pizz|eataly|bagel|gelateria|ramen|demetres|wingstop|waffles|shakeshack|bhc|cocacola|restaur)", desc):
-        return "Food & Dining"
-    elif re.search(r"(dollarama|shein|hudsonsbay|yorkdale|amzn|shop|bodyworks|zara|thrift|valuevillage|kiokii|uniqlo|arden|ikea|apple|dollarstore)", desc):
-        return "Shopping"
-    elif re.search(r"(newnham|campus|onecard)", desc):
-        return "Campus Cafeteria"
-    else:
-        return "Other"
-    
 st.title("Personal Spending Analyzer")
 st.write("Upload your credit card CSV file and get categorized insights!")
 
