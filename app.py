83f9d1ab8579b8cc8d60601759cf9671972de54d
ouidhi <136058938+ouidhi@users.noreply.github.com>
2026-01-18 19:55:23 -0500

    fix device config

--- app.py
+++ app.py
@@ -13,6 +13,7 @@ class BertVectorizer(TransformerMixin):
     """BERT vectorizer for text embeddings"""
     def __init__(self, model_name='all-MiniLM-L6-v2'):
         self.model = SentenceTransformer(model_name)
+        self.model = self.model.to('cpu')
 
     def fit(self, X, y=None):
         return self
